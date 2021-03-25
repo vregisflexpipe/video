a993c9648d21497080c1f6ff6ab3d23d041185eb
vregisflexpipe <81382032+vregisflexpipe@users.noreply.github.com>
2021-03-25 13:53:55 -0400

    Create video.js

---
+++ video.js
@@ -0,0 +1,44 @@
+var tag = document.createElement('script');
+            tag.src = "http://www.youtube.com/player_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            var player;
+            function onYouTubePlayerAPIReady() {
+                player = new YT.Player('player', {
+                    height: '390',
+                    width: '640',
+                    videoId: 'YDAXwJrbHzM'
+                });
+            }
+            $(window).load(function(){
+                jQuery('#Titre1').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(25);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre2').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(60);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre3').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(90);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre4').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
