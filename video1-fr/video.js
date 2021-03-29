c4de9cb1c628589035ccde481bf88ad8157dff2c
vregisflexpipe <81382032+vregisflexpipe@users.noreply.github.com>
2021-03-29 16:10:53 -0400

    Add files via upload

---
+++ video1-fr/video.js
@@ -0,0 +1,100 @@
+var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/player_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            var player;
+            function onYouTubePlayerAPIReady() {
+                player = new YT.Player('player', {
+                    height: '470',
+                    width: '834',
+                    videoId: 'YDAXwJrbHzM'
+                });
+            }
+            $(window).load(function(){
+                jQuery('#Titre1').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(25);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre2').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(60);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre3').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(90);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre4').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre5').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre6').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre7').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre8').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre9').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre10').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre11').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(120);
+                    return false;
+                });
+            });
