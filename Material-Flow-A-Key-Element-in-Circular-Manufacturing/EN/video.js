56c584772d012a3c1b20161efe5a75fb96a1d056
Jonatanflex <90857123+Jonatanflex@users.noreply.github.com>
2021-09-16 12:59:31 -0400

    Add files via upload

---
+++ Material-Flow-A-Key-Element-in-Circular-Manufacturing/EN/video.js
@@ -0,0 +1,53 @@
+var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/player_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            var player;
+            function onYouTubePlayerAPIReady() {
+                player = new YT.Player('player', {
+                    height: '470',
+                    width: '834',
+                    videoId: 'HGn9Jkmaq48'
+                });
+            }
+            $(window).load(function(){
+                jQuery('#Titre1').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(43);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre2').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(188);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre3').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(336);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre4').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(422);
+                    return false;
+                });
+            });
+            $(window).load(function(){
+                jQuery('#Titre5').click(function(){
+                    //console.log('test');
+                    console.log(player);
+                    player.seekTo(522);
+                    return false;
+                });
+            });
+           
